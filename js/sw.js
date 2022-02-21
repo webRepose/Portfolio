44f0aca70c1f62a03d579ac13e92bb6526d87063
Daniel <danilagalakov3@gmail.com>
2022-02-21 10:44:48 +0600

    manifest

--- js/sw.js
+++ js/sw.js
@@ -1,13 +1,13 @@
 const cachName = 'cachApp'
 const assetURL = [
-    '/portfolio/portfolio.html',
-    '/portfolio/ru.html',
-    '/portfolio/styles/adapt.css',
-    '/portfolio/styles/style.css',
-    '/portfolio/styles/components/footer.css',
-    '/portfolio/styles/components/header.css',
-    '/portfolio/styles/components/main.css',
-    '/portfolio/js/index.js'
+    '/portfolio.html',
+    '/ru.html',
+    '/styles/adapt.css',
+    '/styles/style.css',
+    '/styles/components/footer.css',
+    '/styles/components/header.css',
+    '/styles/components/main.css',
+    '/js/index.js'
 ]
 
 self.addEventListener('install', event => {
