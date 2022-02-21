dc4525d3c2498996bd2e8ba5ea42a0e8a4e391eb
Daniel <danilagalakov3@gmail.com>
2022-02-21 10:12:35 +0600

    manifest

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,19 @@
+const cachName = 'cachApp'
+const assetURL = [
+    'portfolio.html',
+    'ru.html',
+    '/styles/adapt.css',
+    '/styles/style.css',
+    '/styles/components/footer.css',
+    '/styles/components/header.css',
+    '/styles/components/main.css',
+    '/js/index.js'
+]
+
 self.addEventListener('install', event => {
-    console.log('[SW]: install')
+    event.waitUntil(
+        caches.open(cachName).then(cache => cache.addAll(assetURL))
+    )
 })
 
 self.addEventListener('activate', event => {
