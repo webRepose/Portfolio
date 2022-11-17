ee3bb7a81e78091fdac060b447abb80e77f1b956
Daniel <danilagalakov3@gmail.com>
2022-11-17 22:20:45 +0600

    skills

--- js/lang.js
+++ js/lang.js
@@ -94,7 +94,8 @@ function ruTitle() {
   document.querySelector('#CSS3').setAttribute('href', 'https://ru.wikipedia.org/wiki/CSS');
   document.querySelector('#SCSS').setAttribute('href', 'https://ru.wikipedia.org/wiki/Sass');
   document.querySelector('#GIT').setAttribute('href', 'https://ru.wikipedia.org/wiki/GitLab');
-  document.querySelector('#JS').setAttribute('href', 'https://ru.wikipedia.org/wiki/Adobe_XD');
+  document.querySelector('#JS').setAttribute('href', 'https://ru.wikipedia.org/wiki/JavaScript');
+  document.querySelector('#XD').setAttribute('href', 'https://ru.wikipedia.org/wiki/Adobe_XD');
   document.querySelector('#contacts-message').setAttribute('title', 'Отправить сообщение');
   document.querySelector('.portfolio-dropDown').setAttribute('title', 'Посмотреть еще!');
   document.querySelector('html').setAttribute('lang', 'ru');
